0d1261ad7eb8f217d611f9043dc9c07dd5e43bd2
raoul <mironraoul@gmail.com>
2021-11-20 21:38:32 +0000

    Make 'contact' template content

---
+++ content/contact.js
@@ -0,0 +1,7 @@
+export default {
+    content: `
+    <h1>Wanna get a hold of me?</h1>
+    <p>Email: <a href="mailto:mironraoul@gmail.com">mironraoul@gmail.com</a></p>
+    <p>Twitter: <a href="https://twitter.com/MironRaoul">https://twitter.com/MironRaoul</a></p>
+    `
+}
