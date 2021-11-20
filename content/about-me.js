067b57db8285afcb8723402eec74f0ac5fcc8fb0
raoul <mironraoul@gmail.com>
2021-11-20 21:37:41 +0000

    Make 'about me' template content with placeholder text

---
+++ content/about-me.js
@@ -0,0 +1,27 @@
+export default {
+   content: `
+   <h1>About Me</h1>
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+   incididunt ut labore et dolore magna aliqua. Congue mauris rhoncus aenean vel 
+   elit scelerisque mauris. Quis imperdiet massa tincidunt nunc pulvinar. Vitae aliquet 
+   nec ullamcorper sit amet risus nullam. Eu consequat ac felis donec et odio pellentesque diam. 
+   Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Sed viverra 
+   ipsum nunc aliquet bibendum. Pellentesque elit eget gravida cum sociis natoque. Viverra 
+   aliquet eget sit amet. Euismod quis viverra nibh cras pulvinar mattis nunc. Et ligula ullamcorper
+   malesuada proin. Orci ac auctor augue mauris augue neque gravida. Euismod lacinia at quis risus
+   sed vulputate odio. Sit amet aliquam id diam maecenas ultricies mi eget mauris. Pretium fusce id 
+   velit ut. Tristique senectus et netus et malesuada. Fermentum iaculis eu non diam. Elementum 
+   sagittis vitae et leo.</p>
+   <p>
+   <p>Consequat mauris nunc congue nisi vitae suscipit tellus mauris. Montes nascetur ridiculus mus mauris.
+   Lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Duis convallis convallis tellus id 
+   interdum. Id venenatis a condimentum vitae sapien pellentesque habitant. Pulvinar proin gravida 
+   hendrerit lectus. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. 
+   Malesuada fames ac turpis egestas maecenas pharetra convallis posuere morbi. Orci phasellus 
+   egestas tellus rutrum tellus. Tempus quam pellentesque nec nam aliquam sem. Volutpat ac tincidunt 
+   vitae semper quis. Nunc scelerisque viverra mauris in aliquam sem fringilla. Condimentum id venenatis
+   a condimentum vitae sapien pellentesque habitant morbi. Nisi porta lorem mollis aliquam ut porttitor.              Integer eget aliquet nibh praesent tristique magna sit amet purus. At ultrices mi tempus imperdiet.
+   Lorem ipsum dolor sit amet consectetur. Lacus laoreet non curabitur gravida arcu ac tortor dignissim.
+   Pretium nibh ipsum consequat nisl vel.</p>
+   `
+}
