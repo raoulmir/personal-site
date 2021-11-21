dcdfa6db4fdf99d8bf3de5f3638526af6d4231fb
raoul <mironraoul@gmail.com>
2021-11-21 23:03:55 +0000

    Make 'intro' template content

---
+++ content/intro.js
@@ -0,0 +1,26 @@
+export default {
+    content: `
+    <div style="display:flex; flex-shrink: 1; align-items:center;">
+        <div style=" min-width: 20%">     
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 24 12" shape-rendering="crispEdges">
+            <metadata>Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj</metadata>
+            <path stroke="#000000" d="M2 0h1M21 0h1M1 1h1M4 1h6M13 1h7M22 1h1M1 2h1M4 2h1M9 2h1M13 2h1M15 2h3M19 2h1M22 2h1M1 3h1M4 3h1M6 3h2M9 3h1M13 3h1M16 3h1M19 3h1M22 3h1M1 4h1M4 4h1M6 4h2M9 4h5M15 4h1M17 4h1M19 4h1M22 4h1M0 5h1M4 5h1M8 5h2M13 5h1M15 5h3M19 5h1M23 5h1M0 6h1M4 6h1M6 6h2M9 6h1M11 6h3M15 6h3M19 6h1M23 6h1M1 7h1M4 7h1M6 7h2M9 7h1M13 7h1M15 7h3M19 7h1M22 7h1M1 8h1M4 8h1M6 8h2M9 8h3M13 8h1M15 8h3M19 8h1M22 8h1M1 9h1M4 9h1M6 9h2M9 9h1M13 9h1M15 9h3M19 9h1M22 9h1M1 10h1M4 10h16M22 10h1M2 11h1M21 11h1" />
+            </svg>
+        </div>
+        <div style="padding: 2rem">
+            <h1 style="margin-bottom: 0;">Raoul-Sebastian Miron</h1>
+            <span style="font-size: 1rem;">v1.0.0</span></br>
+            <span>Occupation: Software Engineer</span><br>
+            <span>Nationality: Romanian</span><br><br>
+            <span>Skills:</span>
+            <ul>
+                <li>JavaScript(Vue.js, Nuxt.js, jQuery, Ajax & ES6)</li>
+                <li>HTML and CSS using pre-processors like Sass(SCSS)</li>
+                <li>PHP(Laravel, 5.6+)</li>
+                <li>Agile Methodologies (Scrum, Kanban)</li>
+                <li>DevOps Certified</li>
+            </ul>
+        </div>
+    </div>
+    `
+}
