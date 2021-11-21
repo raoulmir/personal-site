e395a6adc74867b872c00cc36c9ce52702d7cda7
raoul <mironraoul@gmail.com>
2021-11-21 23:04:11 +0000

    Import 'intro' template content into index

--- content/index.js
+++ content/index.js
@@ -1,3 +1,4 @@
 export {default as aboutMe} from './about-me.js'
 export {default as contact} from './contact.js'
 export {default as photo} from './photo.js'
+export {default as intro} from './intro.js'
