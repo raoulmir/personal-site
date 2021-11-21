efabf2eb4cb0327b9963f0262e2fa8d53d13206c
raoul <mironraoul@gmail.com>
2021-11-21 23:05:24 +0000

    Add links to contact

--- content/contact.js
+++ content/contact.js
@@ -3,5 +3,7 @@ export default {
     <h1>Wanna get a hold of me?</h1>
     <p>Email: <a href="mailto:mironraoul@gmail.com">mironraoul@gmail.com</a></p>
     <p>Twitter: <a href="https://twitter.com/MironRaoul">https://twitter.com/MironRaoul</a></p>
+    <p>Instagram: <a href="https://www.instagram.com/raoul.miron/">https://www.instagram.com/raoul.miron/</a></p>
+    <p>LinkedIn: <a href="https://www.linkedin.com/in/raoul-m-87a038b1/">https://www.linkedin.com/in/raoul-m-87a038b1/</a></p>
     `
 }
