ec65ca1927b98a8055bc752c004208aa07262302
raoul <mironraoul@gmail.com>
2021-11-21 23:04:58 +0000

    Add logo with link to index to navigation

--- components/navigation.js
+++ components/navigation.js
@@ -11,6 +11,12 @@ export const Navigation = {
     },
     emits: ['set-route'],
     template: `
+        <a class="home" href="/">
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 -0.5 15 15" shape-rendering="crispEdges">
+            <metadata>Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj</metadata>
+            <path stroke="#000000" d="M7 0h1M6 1h3M5 2h5M4 3h7M3 4h9M2 5h11M1 6h13M0 7h15M1 8h13M1 9h13M1 10h5M9 10h5M1 11h5M9 11h5M1 12h5M9 12h5M1 13h5M9 13h5M1 14h5M9 14h5" />
+            </svg>
+        </a>
         <div v-for="option in options">
             <div v-if="option.external">
                 <a :href="option.link">
