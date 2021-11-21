a0c42f77f3596174b00662f7696c7e1518ede7e5
raoul <mironraoul@gmail.com>
2021-11-21 23:01:25 +0000

    Make 'photo' template content

---
+++ content/photo.js
@@ -0,0 +1,7 @@
+export default {
+    content: `
+    <div style="
+        background: url(https://media-exp1.licdn.com/dms/image/C4D03AQGRduix1hw5Eg/profile-displayphoto-shrink_800_800/0/1636111095482?e=1643241600&v=beta&t=Yd28EWv3RWvxxhpYE-ODfris7w6kmBEBsgSomY_lC30)">
+    </div>
+    `
+}
