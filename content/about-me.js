7cbd4d5716f58aa85d3c981cbf8856f6fbb2d3a1
raoul <mironraoul@gmail.com>
2022-05-11 17:33:03 +0100

    Update about me section with real text

--- content/about-me.js
+++ content/about-me.js
@@ -1,27 +1,16 @@
 export default {
    content: `
    <h1>About Me</h1>
-   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-   incididunt ut labore et dolore magna aliqua. Congue mauris rhoncus aenean vel 
-   elit scelerisque mauris. Quis imperdiet massa tincidunt nunc pulvinar. Vitae aliquet 
-   nec ullamcorper sit amet risus nullam. Eu consequat ac felis donec et odio pellentesque diam. 
-   Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Sed viverra 
-   ipsum nunc aliquet bibendum. Pellentesque elit eget gravida cum sociis natoque. Viverra 
-   aliquet eget sit amet. Euismod quis viverra nibh cras pulvinar mattis nunc. Et ligula ullamcorper
-   malesuada proin. Orci ac auctor augue mauris augue neque gravida. Euismod lacinia at quis risus
-   sed vulputate odio. Sit amet aliquam id diam maecenas ultricies mi eget mauris. Pretium fusce id 
-   velit ut. Tristique senectus et netus et malesuada. Fermentum iaculis eu non diam. Elementum 
-   sagittis vitae et leo.</p>
-   <p>
-   <p>Consequat mauris nunc congue nisi vitae suscipit tellus mauris. Montes nascetur ridiculus mus mauris.
-   Lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Duis convallis convallis tellus id 
-   interdum. Id venenatis a condimentum vitae sapien pellentesque habitant. Pulvinar proin gravida 
-   hendrerit lectus. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. 
-   Malesuada fames ac turpis egestas maecenas pharetra convallis posuere morbi. Orci phasellus 
-   egestas tellus rutrum tellus. Tempus quam pellentesque nec nam aliquam sem. Volutpat ac tincidunt 
-   vitae semper quis. Nunc scelerisque viverra mauris in aliquam sem fringilla. Condimentum id venenatis
-   a condimentum vitae sapien pellentesque habitant morbi. Nisi porta lorem mollis aliquam ut porttitor.              Integer eget aliquet nibh praesent tristique magna sit amet purus. At ultrices mi tempus imperdiet.
-   Lorem ipsum dolor sit amet consectetur. Lacus laoreet non curabitur gravida arcu ac tortor dignissim.
-   Pretium nibh ipsum consequat nisl vel.</p>
+   <p>I'm a professional and, experienced full-stack software engineer. 
+   I began my journey with nothing but BTEC course in IT and some personal learning in the web space, starting as a Junior Web Developer and, within a matter of 4 years reaching seniour level.
+   Through this journey I've seen a lot; a company slowly moving away from a very dated waterfall style development process, beginnign to integrate proper CD/CI and becoming Agile (through which I learned a lot myself)
+   and I've been involved with many different parts of the company, working on a variety of public facing and enterprise websites and web applications. Even the stack and approach on these were widely varied; 
+   I've had meaningful exposure to all sorts, from legacy websites running PHP 5.6 with jQuery on the front-end, to full MVC Laravel applications, extensive web APIs, and more modern Javascript single-page applications with Vue.js and Nuxt.js. At one point I've even had to build some web/mobile hybrid apps.
+   <p>Majority of my expertise these days are in API development and modern front end applications build using Vue/Nuxt, but I've proven to be quite versetile and adaptible in my skills.</p>
+   <p>I'm now also at a stage where I provide support to newer and, junior software engineers, either through direct guidance or a pull request process. 
+   I've been told in many scenarious I'm a great person to work and communicate with and I've managed to exapnd my "people" skills more than I would've imagined in a programming career. I've even been a Scrum Master</p>
+   <p>As a person, I find myself enjoying hoping from on thing to another. I simply love trying new things and hobbies, and some stick around, while others go back on the shelf to gather dust until I'm interested again.
+   I love coding, playing and experimenting with tech, taking photos on my travels and editing them, as well as more passive hobbies such as movies, books and videogames. 
+   For other forms of recration, I also enjoy going to venues and music festivals and I won't shy away from a pint or two. </p>
    `
 }
