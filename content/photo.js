8dc157dc849b583c293b4c95d49bc0106e35e740
raoul <mironraoul@gmail.com>
2022-05-11 17:31:23 +0100

    Update photo link in photo component

--- content/photo.js
+++ content/photo.js
@@ -1,7 +1,7 @@
 export default {
     content: `
     <div style="
-        background: url(https://lh3.googleusercontent.com/lBM_1_eWJWOk_RTD05zcyWY2K5A-mz2LwBBgFGRAjpTQbcmZEwIpn-oheSPKeQS3IIh7Yxvs8lONkbVbUUOm=w1920-h890)">
+        background: url(https://raoul-personal-site.s3.eu-west-2.amazonaws.com/me.jpg)">
     </div>
     `
 }
